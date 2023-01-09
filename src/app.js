85ba9533b93c84e4593083206dffca2fa7acc9c6
lucas-palmeida <lucaspacheco95@gmail.com>
2023-01-09 11:56:05 -0300

    refact: comentario nos dados de teste para avaliacao do hub

--- src/app.js
+++ src/app.js
@@ -8,99 +8,99 @@ server.use(cors());
 server.use(json());
 
 const usuarios = [
-  {
-    username: "sasukeuchiha",
-    avatar: "https://upload.wikimedia.org/wikipedia/pt/0/02/Sasukereup.jpg",
-  },
-  {
-    username: "narutouzumaki",
-    avatar:
-      "https://viciados.net/wp-content/uploads/2022/11/Naruto-Shippuden-Boruto-2023.webp",
-  },
-  {
-    username: "kakashihatake",
-    avatar:
-      "https://quartoframe.com.br/wp-content/uploads/2021/07/kakashi-hatake-capa.jpg",
-  },
-  {
-    username: "itachiuchiha",
-    avatar:
-      "https://upload.wikimedia.org/wikipedia/pt/thumb/e/e5/Itachi_Uchiha.jpg/200px-Itachi_Uchiha.jpg",
-  },
-  {
-    username: "shikamarunara",
-    avatar:
-      "http://pm1.narvii.com/6370/9deb6493996e783b0c0d034b89ccbc2e67de4fca_00.jpg",
-  },
-  {
-    username: "madarauchiha",
-    avatar:
-      "https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/10/Jump-Force-1.jpg",
-  },
-  {
-    username: "jiraiya",
-    avatar:
-      "https://i.pinimg.com/originals/97/c8/49/97c84938fa55449c6d7cc2799cec7f70.jpg",
-  },
+  // {
+  //   username: "sasukeuchiha",
+  //   avatar: "https://upload.wikimedia.org/wikipedia/pt/0/02/Sasukereup.jpg",
+  // },
+  // {
+  //   username: "narutouzumaki",
+  //   avatar:
+  //     "https://viciados.net/wp-content/uploads/2022/11/Naruto-Shippuden-Boruto-2023.webp",
+  // },
+  // {
+  //   username: "kakashihatake",
+  //   avatar:
+  //     "https://quartoframe.com.br/wp-content/uploads/2021/07/kakashi-hatake-capa.jpg",
+  // },
+  // {
+  //   username: "itachiuchiha",
+  //   avatar:
+  //     "https://upload.wikimedia.org/wikipedia/pt/thumb/e/e5/Itachi_Uchiha.jpg/200px-Itachi_Uchiha.jpg",
+  // },
+  // {
+  //   username: "shikamarunara",
+  //   avatar:
+  //     "http://pm1.narvii.com/6370/9deb6493996e783b0c0d034b89ccbc2e67de4fca_00.jpg",
+  // },
+  // {
+  //   username: "madarauchiha",
+  //   avatar:
+  //     "https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/10/Jump-Force-1.jpg",
+  // },
+  // {
+  //   username: "jiraiya",
+  //   avatar:
+  //     "https://i.pinimg.com/originals/97/c8/49/97c84938fa55449c6d7cc2799cec7f70.jpg",
+  // },
 ];
 
 const tweets = [
-  {
-    username: "sasukeuchiha",
-    tweet: "Desde que você não desista, vai sempre existir salvação.",
-  },
-  {
-    username: "narutouzumaki",
-    tweet: "Desista de me fazer desistir!",
-  },
-  {
-    username: "kakashihatake",
-    tweet: "Saber o que é certo e escolher ignorá-lo é um ato de covardia.",
-  },
-  {
-    username: "itachiuchiha",
-    tweet: "Você é fraco. Te falta ódio.",
-  },
-  {
-    username: "shikamarunara",
-    tweet: "Não há vantagem alguma em viver a vida correndo.",
-  },
-  {
-    username: "madarauchiha",
-    tweet: "Se você se concentrar em algo, poderá fazer qualquer coisa.",
-  },
-  {
-    username: "jiraiya",
-    tweet: "Quando você conhece a dor e o sentimento, não machuca as pessoas.",
-  },
-  {
-    username: "sasukeuchiha",
-    tweet: "Desde que você não desista, vai sempre existir salvação.",
-  },
-  {
-    username: "narutouzumaki",
-    tweet: "Desista de me fazer desistir!",
-  },
-  {
-    username: "kakashihatake",
-    tweet: "Saber o que é certo e escolher ignorá-lo é um ato de covardia.",
-  },
-  {
-    username: "itachiuchiha",
-    tweet: "Você é fraco. Te falta ódio.",
-  },
-  {
-    username: "shikamarunara",
-    tweet: "Não há vantagem alguma em viver a vida correndo.",
-  },
-  {
-    username: "madarauchiha",
-    tweet: "Se você se concentrar em algo, poderá fazer qualquer coisa.",
-  },
-  {
-    username: "jiraiya",
-    tweet: "Quando você conhece a dor e o sentimento, não machuca as pessoas.",
-  },
+  // {
+  //   username: "sasukeuchiha",
+  //   tweet: "Desde que você não desista, vai sempre existir salvação.",
+  // },
+  // {
+  //   username: "narutouzumaki",
+  //   tweet: "Desista de me fazer desistir!",
+  // },
+  // {
+  //   username: "kakashihatake",
+  //   tweet: "Saber o que é certo e escolher ignorá-lo é um ato de covardia.",
+  // },
+  // {
+  //   username: "itachiuchiha",
+  //   tweet: "Você é fraco. Te falta ódio.",
+  // },
+  // {
+  //   username: "shikamarunara",
+  //   tweet: "Não há vantagem alguma em viver a vida correndo.",
+  // },
+  // {
+  //   username: "madarauchiha",
+  //   tweet: "Se você se concentrar em algo, poderá fazer qualquer coisa.",
+  // },
+  // {
+  //   username: "jiraiya",
+  //   tweet: "Quando você conhece a dor e o sentimento, não machuca as pessoas.",
+  // },
+  // {
+  //   username: "sasukeuchiha",
+  //   tweet: "Desde que você não desista, vai sempre existir salvação.",
+  // },
+  // {
+  //   username: "narutouzumaki",
+  //   tweet: "Desista de me fazer desistir!",
+  // },
+  // {
+  //   username: "kakashihatake",
+  //   tweet: "Saber o que é certo e escolher ignorá-lo é um ato de covardia.",
+  // },
+  // {
+  //   username: "itachiuchiha",
+  //   tweet: "Você é fraco. Te falta ódio.",
+  // },
+  // {
+  //   username: "shikamarunara",
+  //   tweet: "Não há vantagem alguma em viver a vida correndo.",
+  // },
+  // {
+  //   username: "madarauchiha",
+  //   tweet: "Se você se concentrar em algo, poderá fazer qualquer coisa.",
+  // },
+  // {
+  //   username: "jiraiya",
+  //   tweet: "Quando você conhece a dor e o sentimento, não machuca as pessoas.",
+  // },
 ];
 
 server.post("/sign-up", (req, res) => {
